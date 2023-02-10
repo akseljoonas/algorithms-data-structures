5627697b687199468ff7ed5c5272ec0e7576c42e
akselj <aksel.reedi@gmail.com>
2023-02-10 19:51:08 +0100

    wadedfa

--- 1planes/planes.c
+++ 1planes/planes.c
@@ -13,8 +13,8 @@
 #include <math.h>    // math functions
 #include <string.h>  // enables string library, e.g. strlen(str)
 
-//#include "stack.h"
-//#include "queue.h"
+#include "stack.h"
+#include "queue.h"
 #define RUNWAY_CAPACITY 7
 #define GARAGE_CAPACITY 5
 
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
 	while (planeIndex != -1) {
 		scanf("%d %s", &planeIndex, repairs);
 
-		if (strcmp(repairs, no)) {
+		if (strcmp(repairs, no)) { // if repairs str is longer than 'no' then push into hangar
 			push(planeIndex, hangar);
 			currentHangarOc++;
 		} else {
@@ -40,21 +40,21 @@ int main(int argc, char *argv[]) {
 		}
 
 		if (currentRunwayOc == RUNWAY_CAPACITY) {
-			for // every plane in the queue, dequeue
+			//for // every plane in the queue, dequeue
 
 		}
 
 		if (currentHangarOc == GARAGE_CAPACITY) {
-			for // every plane in the queue, dequeue
-			for // every plane in the hangar, add to queue
+			//for // every plane in the queue, dequeue
+			//for // every plane in the hangar, add to queue
 			// kas sa näed seda v?
 			// 2
 		}
 	}
 
 	if (currentHangarOc > 0) {
-		for // every plane in the hangar, add to queue
-		for // very plane in the queue, dequeue
+		//for // every plane in the hangar, add to queue
+		//for // very plane in the queue, dequeue
 	}
 
 	return 0;
