66d9850f9be72d0745a05edf8c3cb13dad55cb7d
akselj <aksel.reedi@gmail.com>
2023-02-10 12:03:10 +0100

    wadeva esimene fail

---
+++ planes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* This imports the type and function declarations for the linear data
+ * structures presented in the first lecture: stacks, queues and lists.
+ *
+ * Feel free to remove any includes you do not need!
+ */
+#include "stack.h"
+#include "queue.h"
+#include "list.h"
+#define RUNWAY_CAPACITY 7
+#define GARAGE_CAPACITY 5
+
+// add your code here
+
+int main(int argc, char *argv[]) {
+	// add your code here
+	return 0;
+}
