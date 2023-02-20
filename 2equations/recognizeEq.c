50097834e7fc403b557501babbf2085770250794
akselj <aksel.reedi@gmail.com>
2023-02-20 18:00:38 +0100

    added recognizeExp.c code from brightspace

--- 2equations/recognizeEq.c
+++ 2equations/recognizeEq.c
@@ -15,3 +15,70 @@
 #include "recognizeEq.h"
 
 
+// start of recognizeExp.c
+int acceptSymbol(char symbol, List **list) {
+	if (*list != NULL && (*list)->type == SYMBOL && ((*list)->token).symbol == symbol) {
+		*list = (*list)->next;
+		return 1;
+	}
+	return 0;
+}
+
+int acceptIdentifier(List **list) {
+	if (*list != NULL && (*list)->type == IDENTIFIER) {
+		*list = (*list)->next;
+		return 1;
+	}
+	return 0;
+}
+
+int acceptNumber(List **list) {
+	if (*list != NULL && (*list)->type == NUMBER) {
+		*list = (*list)->next;
+		return 1;
+	}
+	return 0;
+}
+
+int acceptFactor(List **list) {
+	if (acceptNumber(list)) {
+		return 1;
+	}
+	if (acceptIdentifier(list)) {
+		return 1;
+	}
+	if (acceptSymbol('(', list)) {
+		return acceptExpression(list) && acceptSymbol(')', list);
+	}
+	return 0;
+}
+
+int acceptTerm(List **list) { // needs change according to excercise
+	if (!acceptFactor(list)) {
+		return 0;
+	}
+	while (acceptSymbol('*', list) || acceptSymbol('/', list)) {
+		// If we encounter a * or /, 
+		// it must be followed by another term
+		if (!acceptFactor(list)) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int acceptExpression(List **list) { // needs change according to excercise
+	if (!acceptTerm(list)) {
+		return 0;
+	}
+	while (acceptSymbol('+', list) || acceptSymbol('-', list)) {
+		// If we encounter a + or -, 
+		// it must be followed by another term
+		if (!acceptTerm(list)) {
+			return 0;
+		}
+	}
+	return 1;
+}
+// end of recognizeExp.c
+
