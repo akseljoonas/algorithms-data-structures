91d9704b511b49075c3c06be819392b035bd146f
akselj <aksel.reedi@gmail.com>
2023-03-14 19:45:28 +0100

    wadeva.txt

--- 4expressions/infixExp.c
+++ 4expressions/infixExp.c
@@ -129,6 +129,49 @@ double valueExpTree(ExpTree *treeNode) {
 	}
 }
 
+int acceptFactor(List **list) {
+	if (acceptNumber(list)) {
+		
+		return 1;
+	}
+	if (acceptIdentifier(list)) {
+		return 1;
+	}
+	if (acceptSymbol('(', list)) {
+		return acceptExpression(list) && acceptSymbol(')', list);
+	}
+	return 0;
+}
+
+int acceptTerm(List **list)  { // pass down a tree
+	if (!acceptFactor(list)) { // build a factor
+		return 0;
+	}
+	while (acceptSymbol('*', list) || acceptSymbol('/', list)) { 
+		// If we encounter a * or /, 
+		// it must be followed by another term
+		if (!acceptFactor(list)) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int acceptExpression(List **list) {
+	if (!acceptTerm(list)) {
+		return 0;
+	}
+	while (acceptSymbol('+', list) || acceptSymbol('-', list)) {
+		// If we encounter a + or -, 
+		// it must be followed by another term
+		if (!acceptTerm(list)) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+
 void prefExpTrees() {
 	char *ar;
 	List *list;
